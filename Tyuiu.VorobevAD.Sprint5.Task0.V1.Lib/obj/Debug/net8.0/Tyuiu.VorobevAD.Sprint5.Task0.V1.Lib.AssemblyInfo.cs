44a473e433f56c097266fc9e01bf79fe495473c6
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-02 16:20:02 +0500

    task0 2

--- Tyuiu.VorobevAD.Sprint5.Task0.V1.Lib/obj/Debug/net8.0/Tyuiu.VorobevAD.Sprint5.Task0.V1.Lib.AssemblyInfo.cs
+++ Tyuiu.VorobevAD.Sprint5.Task0.V1.Lib/obj/Debug/net8.0/Tyuiu.VorobevAD.Sprint5.Task0.V1.Lib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.VorobevAD.Sprint5.Task0.V1.Lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e713e8cfcafd7e151d3fc168f80576b4b9e55b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.VorobevAD.Sprint5.Task0.V1.Lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.VorobevAD.Sprint5.Task0.V1.Lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
