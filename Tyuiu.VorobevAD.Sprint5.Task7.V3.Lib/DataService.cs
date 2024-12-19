9d2452c5ed86f9f81da826ea4257e5ce9cea7539
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-19 18:37:48 +0500

    lakjdflakdjfiie

--- Tyuiu.VorobevAD.Sprint5.Task7.V3.Lib/DataService.cs
+++ Tyuiu.VorobevAD.Sprint5.Task7.V3.Lib/DataService.cs
@@ -6,7 +6,27 @@ public class DataService : tyuiu.cources.programming.interfaces.Sprint5.ISprint5
 {
 	public string LoadDataAndSave(string path)
 	{
-		return "Hello, !  is my first program.";
+		string str = File.ReadAllText(path);
+		string[] slovo = str.Split(' ');
+
+		string temp = "";
+		for (int i = 0; i < slovo.Length; i++)
+		{
+			if (slovo[i].Length == 2) slovo[i] = "XY";
+
+			if (i != slovo.Length - 1) temp += slovo[i] + ' ';
+			else temp += slovo[i];
+		}
+
+		string OutPut = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V16.txt");
+
+		FileInfo fileInfo = new(OutPut);
+		bool fileExists = fileInfo.Exists;
+		if (fileExists) File.Delete(OutPut);
+
+		File.AppendAllText(OutPut, temp);
+
+		return OutPut;
 		throw new NotImplementedException();
 	}
 }
