69b33a777e57226d0a294bd2bbfe63e9724d2e30
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-19 17:57:09 +0500

    a;lfdjka;lsdf

---
+++ Tyuiu.VorobevAD.Sprint5.Task6.V24.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.VorobevAD.Sprint5.Task6.V24.Lib;
+
+public class DataService : tyuiu.cources.programming.interfaces.Sprint5.ISprint5Task6V24
+{
+	public int LoadFromDataFile(string path)
+	{
+		return 42;
+		throw new NotImplementedException();
+	}
+}
