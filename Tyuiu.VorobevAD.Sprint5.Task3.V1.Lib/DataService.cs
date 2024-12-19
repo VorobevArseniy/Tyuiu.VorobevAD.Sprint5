d0646ff8675bf7525ee318dec66915af9a4c52e7
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-19 16:10:30 +0500

    piopoi

---
+++ Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib;
+
+public class DataService : tyuiu.cources.programming.interfaces.Sprint5.ISprint5Task3V1
+{
+	public string SaveToFileTextData(int x)
+	{
+		return "test";
+		throw new NotImplementedException();
+	}
+}
