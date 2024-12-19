c24ff4879dbca6a845e739095f3a9b025d8bc551
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-19 17:43:16 +0500

    sdfsdf

---
+++ Tyuiu.VorobevAD.Sprint5.Task5.V8.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.VorobevAD.Sprint5.Task5.V8.Lib;
+
+public class DataService : tyuiu.cources.programming.interfaces.Sprint5.ISprint5Task5V8
+{
+	public double LoadFromDataFile(string path)
+	{
+		return 0.1;
+		throw new NotImplementedException();
+	}
+}
---
+++ Tyuiu.VorobevAD.Sprint5.Task5.V8.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib;
+
+namespace Tyuiu.VorobevAD.Sprint5.Task3.V1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new();
+
+        Console.WriteLine(ds.SaveToFileTextData(3));
+    }
+}
