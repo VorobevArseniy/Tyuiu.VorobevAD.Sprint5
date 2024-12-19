f48eeea14093dcad1b66352ad474e61310f1f9da
uiuiuiui15 <dr.venitus@gmail.com>
2024-12-19 17:39:16 +0500

    asfasdfasdf

--- Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib/obj/Debug/net8.0/Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib.AssemblyInfo.cs
+++ Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib/obj/Debug/net8.0/Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5077098ea0893b583fa5ce9f88ff67eb0e657513")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca80f1b1f627e1261e882238e2b26bacafb6dc0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.VorobevAD.Sprint5.Task3.V1.Lib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Tyuiu.VorobevAD.Sprint5.Task4.V22.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.VorobevAD.Sprint5.Task4.V22.Lib;
+
+public class DataService : tyuiu.cources.programming.interfaces.Sprint5.ISprint5Task4V22
+{
+	public double LoadFromDataFile(string path)
+	{
+		return 0.01;
+		throw new NotImplementedException();
+	}
+}
